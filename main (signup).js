25c3e1e3ca77b48f0a1057c6391d1b245609fe25
unknown <ayenitosin03@gmail.com>
2021-04-29 11:32:34 +0100

    initial commit

---
+++ main (signup).js
@@ -0,0 +1,111 @@
+function setFormMessage(formElement, type, message){
+    const messageElement = formElement.querySelector(".form-message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("message-success", "message-error")
+    messageElement.classList.add('form-message ${type}');
+}
+
+let signupForm = document.querySelector("#signup");
+document.addEventListener("DOMContentLoaded", () => {
+     
+     const signupForm = document.querySelector("#signup");
+
+document.querySelector("#linkCreateAccount").addEventListener("click", e   => {
+    e.preventDefault();
+    loginForm.classList.add("form-hidden");
+    signupForm.classList.remove("form-hidden");
+});
+
+document.querySelector("#linkLogin").addEventListener("click", e => {
+    e.preventDefault();
+    loginForm.classList.remove("form-hidden");
+    signupForm.classList.add("form-hidden");
+});
+
+loginForm.addEventListener("submit", e => {
+   e.preventDefault();
+
+   //setFormMessage(loginForm, "error", "Invalid username/password combination");
+
+});
+
+});
+
+
+
+const displayAlert = ( message) => {
+    const container = document.querySelector('.alert-container');
+    const h5 = document.createElement('h5');
+    h5.className = 'alert';
+    h5.textContent = message;
+    container.append(h5);
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+  };
+
+document.getElementById('signup').addEventListener('submit',async(event)=>{
+    event.preventDefault();
+console.log("Sign up");
+    signUpUser();
+    // const name = document.getElementById('name').value;
+	
+ 
+})
+
+async function signUpUser(){
+ let name = document.getElementById('name').value;
+ let email = document.getElementById('email').value;
+ let password = document.getElementById('password').value;
+
+console.log(name);
+console.log(email);
+console.log(password);
+ if( !email ||! password){
+        console.log("Sorry feilds can not be empty");
+        
+        return;
+    }
+    const data ={
+        email,
+        password,
+name
+    };
+    const url = 'https://kobowork.herokuapp.com/api/v1/auth/register' ;
+
+    
+    const res = await makeFetch(data, url);
+    console.log(res);
+    if(res.error != undefined){
+      //  displayAlert(res.error)
+	setFormMessage(signupForm ,"error", "An Error Occurred While Signing up");
+    } else{
+        location.href = '\index (home).html'
+    }
+
+}
+
+
+
+const makeFetch = async(data, url)=>{
+    try{
+
+        const res = await fetch(url,{
+            method:'POST',
+            headers:{
+                'Content-type':'application/json',
+            },
+            body:JSON.stringify(data)
+        });
+
+        const datas = await res.json();
+        console.log(datas)
+        return datas;
+    }
+    catch (err){
+        return err;
+	console.error(err);
+    }
+
+}
+
+
